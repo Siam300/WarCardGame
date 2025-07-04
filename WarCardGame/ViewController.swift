4872bab38c674f4c600ec6504045b07b25d83ca3
Siam300 <89306483+Siam300@users.noreply.github.com>
2025-07-04 18:34:03 +0600

    Card randomization function added and cards image updates.

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func dealTapped(_ sender: Any) {
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        
+        print("DEBUG: Left number \(leftNumber) and Right number \(rightNumber)")
+        
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        print("DEBUG: Deal button tapped.")
+    }
+    
 
 }
 
